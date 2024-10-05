4cb96c0abf8e9cf4138675f7b8ef0f77f9839bd0
MoisesElias123 <moisesfelipedapaz@gmail.com>
2024-10-05 03:01:45 -0300

    Minha primeira mensagem de commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import Header from "./components/Header/Header";
+import "./styles/App.scss";
+
+function App() {
+  return (
+    <div>
+      <Header />
+    </div>
+  );
+}
+
+export default App;

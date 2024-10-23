20e60f32da9c5c38be36e770eaf8bd897f27d006
MoisesElias123 <moisesfelipedapaz@gmail.com>
2024-10-23 01:04:04 -0300

    refactor: :recycle: move a imagem do logo da pasta public para src/assets para melhor organização
    A logo foi movida de public para src/assets para manter uma melhor organização de arquivos.  Também foi necessário ajustar os componentes que faziam uso da logo, atualizando os imports  para o novo caminho em @assets/default_transparent_765x625.png

--- src/components/Header/parts/Logo/index.jsx
+++ src/components/Header/parts/Logo/index.jsx
@@ -1,9 +1,11 @@
 import "./styles.logo.scss";
+import logo from "@assets/default_transparent_765x625.png";
+
 export function Logo() {
   return (
     <div className="logo">
       <a href="/">
-        <img src="/default_transparent_765x625.png" alt="Logo" />
+        <img src={logo} alt="Logo" />
       </a>
     </div>
   );

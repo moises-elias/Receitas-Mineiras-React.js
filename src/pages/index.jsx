2e3973b38f37b6abf0ad3ce0dd98b94bc57a6f14
MoisesElias123 <moisesfelipedapaz@gmail.com>
2024-12-30 16:11:57 -0300

    fix: corrige erro de import de página que não existe.

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -3,4 +3,3 @@ export { ErrorPage } from "./ErrorPage";
 export { Recipe } from "./Recipe";
 export { SurpriseRecipe } from "./SurpriseRecipe";
 export { UnderConstructionPage } from "./UnderConstructionPage";
-export { Login } from "../components/Auth";
